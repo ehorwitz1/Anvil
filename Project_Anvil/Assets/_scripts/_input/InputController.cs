4b74d185f6adeaf02fb382c592fed4da12511c10
klittell01 <klittell@uwyo.edu>
2018-02-15 13:05:51 -0700

    added model selection and movement

---
+++ Project_Anvil/Assets/_scripts/_input/InputController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// only works with one model for now
+
+public class InputController : MonoBehaviour {
+
+	bool isSelected = false;
+	Plane myPlane;
+	Vector3 targetPoint;
+	public ModelMove ModelMove;
+
+	// Use this for initialization
+	void Start () {
+		myPlane = new Plane(Vector3.up, transform.position);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonUp(0)){
+	    	RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		//	Debug.DrawRay(Camera.main.ScreenPointToRay(Input.mousePosition), Color.red, 0.5f);
+			if (Physics.Raycast(ray, out hit)){ // get the object using a raycast
+				if (hit.transform.name == "Ground"){ // if statement to move object, assuming it is selected
+					float hitDist;
+					if (myPlane.Raycast(ray, out hitDist)){
+						targetPoint = ray.GetPoint(hitDist);
+						Debug.Log(targetPoint);
+						if(ModelMove != null){
+							ModelMove.MoveToPoint(targetPoint);
+						}
+					}
+				} else if (hit.transform.tag == "Model"){ // if statement to select model objects
+						ModelMove = hit.transform.gameObject.GetComponent<ModelMove>();
+					if(!isSelected){
+						isSelected = true;
+						string selectedName; 
+						Transform theTarget;
+						selectedName = hit.collider.name;
+						theTarget = hit.transform;
+						ModelMove.OnSelect(theTarget, selectedName);
+					} else {
+						string selectedName; 
+						Transform theTarget;
+						selectedName = hit.collider.name;
+						theTarget = hit.transform;
+						ModelMove.OnSelect(theTarget, selectedName);
+						ModelMove = null;
+						isSelected = false;
+					}
+				}
+			}
+        }
+    }
+}
---
+++ Project_Anvil/Assets/_scripts/_input/ModelMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this script must be attached to the models
+
+public class ModelMove : MonoBehaviour {
+	
+	Vector3 offset;
+	GameObject textPrefab;
+	GameObject hoverText;
+	bool move = false;
+	bool isSelected = false;
+	Vector3 targetPoint;
+
+	// Use this for initialization
+	void Start () {
+		offset = Vector3.up;
+		textPrefab = Resources.Load("PrefabText") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (move) {
+			transform.position = Vector3.Slerp (transform.position, targetPoint, Time.deltaTime * 2.0f); 
+		}
+
+	}
+
+	public void OnSelect(Transform theTarget, string selectedName){
+		Debug.Log("okay");
+		if(!isSelected){
+	    	hoverText = Instantiate(textPrefab, new Vector3 (theTarget.position.x,
+															 theTarget.position.y + 2,
+															 theTarget.position.z), Quaternion.identity);
+			hoverText.transform.parent = theTarget;
+			hoverText.GetComponent<TextMesh>().text = selectedName;
+			isSelected = true;
+		} else {
+			Destroy(hoverText);
+			isSelected = false;
+		}
+    }
+
+    public void MoveToPoint(Vector3 point){
+    	Debug.Log("the point is " + point);
+    	targetPoint = point;
+    	targetPoint.y += 2;
+    	move = true;
+	}
+}
