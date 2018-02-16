7fa088b80a2c20c825a6722d7afabec5b766dc01
mzrieker <mzrieker@gmail.com>
2018-02-16 06:39:00 -0700

    Added 2D Gameplay
    Also updated model movement to use Lerp instead of Slerp.

---
+++ Project_Anvil/Assets/_scripts/_input/InputController2D.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// only works with one model for now
+
+public class InputController2D : MonoBehaviour {
+
+	bool isSelected = false;
+	Plane myPlane;
+	Vector3 targetPoint;
+	public ModelMove2D ModelMove2D;
+
+	// Use this for initialization
+	void Start () {
+		myPlane = new Plane(Vector3.up, transform.position);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp(0)){
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			//	Debug.DrawRay(Camera.main.ScreenPointToRay(Input.mousePosition), Color.red, 0.5f);
+			if (Physics.Raycast(ray, out hit)){ // get the object using a raycast
+				if (hit.transform.name == "Ground"){ // if statement to move object, assuming it is selected
+					float hitDist;
+					if (myPlane.Raycast(ray, out hitDist)){
+						targetPoint = ray.GetPoint(hitDist);
+						Debug.Log(targetPoint);
+						if(ModelMove2D != null){
+							ModelMove2D.MoveToPoint(targetPoint);
+						}
+					}
+				} else if (hit.transform.tag == "Model"){ // if statement to select model objects
+					ModelMove2D = hit.transform.gameObject.GetComponent<ModelMove2D>();
+					if(!isSelected){
+						isSelected = true;
+						string selectedName; 
+						Transform theTarget;
+						selectedName = hit.collider.name;
+						theTarget = hit.transform;
+						ModelMove2D.OnSelect(theTarget, selectedName);
+					} else {
+						string selectedName; 
+						Transform theTarget;
+						selectedName = hit.collider.name;
+						theTarget = hit.transform;
+						ModelMove2D.OnSelect(theTarget, selectedName);
+						ModelMove2D = null;
+						isSelected = false;
+					}
+				}
+			}
+		}
+	}
+}
--- Project_Anvil/Assets/_scripts/_input/ModelMove2D.cs
+++ Project_Anvil/Assets/_scripts/_input/ModelMove2D.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 // this script must be attached to the models
 
-public class ModelMove : MonoBehaviour {
-	
+public class ModelMove2D : MonoBehaviour {
+
 	Vector3 offset;
 	GameObject textPrefab;
 	GameObject hoverText;
@@ -18,34 +18,38 @@ public class ModelMove : MonoBehaviour {
 		offset = Vector3.up;
 		textPrefab = Resources.Load("PrefabText") as GameObject;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (move) {
+		if (isSelected & move) {
 			transform.position = Vector3.Lerp (transform.position, targetPoint, Time.deltaTime * 2.0f); 
-		}
+		} 
 
+		else {
+			targetPoint = transform.position;
+		}
 	}
 
 	public void OnSelect(Transform theTarget, string selectedName){
 		Debug.Log("okay");
 		if(!isSelected){
-	    	hoverText = Instantiate(textPrefab, new Vector3 (theTarget.position.x + 2,
-															 theTarget.position.y,
-															 theTarget.position.z), Quaternion.identity);
+			hoverText = Instantiate(textPrefab, new Vector3 (theTarget.position.x,
+																theTarget.position.y,
+																theTarget.position.z + 2), Quaternion.identity);
 			hoverText.transform.parent = theTarget;
 			hoverText.GetComponent<TextMesh>().text = selectedName;
+			hoverText.transform.Rotate(new Vector3(90,0,0));
 			isSelected = true;
 		} else {
 			Destroy(hoverText);
 			isSelected = false;
 		}
-    }
+	}
 
-    public void MoveToPoint(Vector3 point){
-    	Debug.Log("the point is " + point);
-    	targetPoint = point;
-    	//targetPoint.y += 2;
-    	move = true;
+	public void MoveToPoint(Vector3 point){
+		Debug.Log("the point is " + point);
+		targetPoint = point;
+		//targetPoint.y += 2;
+		move = true;
 	}
 }
