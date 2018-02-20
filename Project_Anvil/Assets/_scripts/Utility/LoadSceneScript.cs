f3b0789da850292bb983e1ba7e951712679202d4
mzrieker <mzrieker@gmail.com>
2018-02-20 11:14:30 -0700

    Cahnged Toggle Switch
    Toggle switch (between scenes) now says "VDL / Map".

--- Project_Anvil/Assets/_scripts/Utility/LoadSceneScript.cs
+++ Project_Anvil/Assets/_scripts/Utility/LoadSceneScript.cs
@@ -8,8 +8,8 @@ public class LoadSceneScript : MonoBehaviour {
 
 	void OnGUI()
 	{
-		GUI.Label (new Rect (Screen.width / 2 - 50, Screen.height - 80, 100, 30), "Current Scene: " + (Application.loadedLevel + 1));
-		if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.height - 50, 100, 40), "Load Scene: " + (SceneToLoad + 1))) 
+		//GUI.Label (new Rect (Screen.width / 2 - 50, Screen.height - 80, 100, 30), "Current Scene: " + (Application.loadedLevel + 1));
+		if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.height - 50, 100, 40), "VDL / Map")) 
 		{
 			Application.LoadLevel (SceneToLoad);
 		}
