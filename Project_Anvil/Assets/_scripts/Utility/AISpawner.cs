4f9acf878bfaa49de83d61956030d971bcd5bb5e
klittell01 <klittell@uwyo.edu>
2018-03-24 12:52:03 -0600

    added random generation and spawning of units using perlin noise (kinda)

---
+++ Project_Anvil/Assets/_scripts/Utility/AISpawner.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISpawner : MonoBehaviour {
+
+    ObjectPooler objectPooler;
+
+    public float x = 50; // x size or the area to spawn the units
+    public float z = 50; // z size of the area to spawn the units
+    float offset;
+    public int count = 100; // number of units to spawn
+    float minDistance = 1.5f; // minimum proximity to one another
+    List<Vector3> positions = new List<Vector3>();
+
+    // Use this for initialization
+    void Start () {
+        objectPooler = ObjectPooler.Instance;
+        offset = x / 2;
+        BeginPlacing();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+	}
+
+    // BeginPlacing //
+    // loops through spawning the units in a randomly generated location
+    void BeginPlacing()
+    {
+        for(int j = 0; j < count; j++)
+        {
+            objectPooler.SpawnFromPool("AIUnit", PlaceStuffRandomly(), Quaternion.identity);
+        }
+    }
+
+    // PlaceStuffRandomly //
+    // returns a new vector3 that is at least the minimum distance from other units
+    public Vector3 PlaceStuffRandomly()
+    {
+
+        Vector3 newPos;
+        bool Nope = false;
+
+        // We want to place #count godly cubes
+        for (int i = 0; i < count; i++)
+        {
+
+            do
+            {
+                //Pick a random new pos ...
+                newPos = new Vector3(Random.value * x, 0, Random.value * z);
+                
+                // We have to ask mister perlin if he thinks this point is ok (And check distances)
+                Nope = !(PerlinThinksItShouldBeThere(newPos) && CouldPlaceItThere(newPos));
+
+            } while (Nope); // This loop will run endlessly, if you try to stuff too many things in a too small area
+            newPos.x = newPos.x - offset;
+            newPos.z = newPos.z - offset;
+            newPos.y = newPos.y + 1;
+            positions.Add(newPos);
+            Debug.Log(newPos);
+            return newPos;
+        }
+        return new Vector3(0, 0, 0);
+
+    }
+    // CouldPlaceItThere //
+    // checks against the previously generated positions to make sure we arent placing two things on top of each other
+    // returns false if the _newPos is too close and true if it isnt too close
+    private bool CouldPlaceItThere(Vector3 _newPos)
+    {
+        // Loop through all positions where we already want to place something
+        for (int i = 0; i < positions.Count; i++)
+            if (Vector3.Distance(positions[i], _newPos) < minDistance) // ... and check if the new point maybe is to close
+                return false;
+        return true;
+    }
+
+
+    // PerlinThinksItShouldBeThere //
+    // gets a perlin value using the new position. compares that to a random value [0 to 1] and if the perlin value is
+    // greater than that random value there should be a unit spawned there so it returns true, false otherwise
+    private bool PerlinThinksItShouldBeThere(Vector3 _newPos)
+    {
+
+        // Basically how fast perlin changes his mind when you ask him for nearby Points
+        float frequency = .8f;
+
+        // Lets ask him what he thinks of the current position
+        float howSurePerlinIsThatItShouldBeThere = Mathf.PerlinNoise(_newPos.x / x * frequency, _newPos.z / z * frequency);
+
+        if (Random.value <= howSurePerlinIsThatItShouldBeThere)
+            return true;
+        else
+            return false;
+    }
+}
---
+++ Project_Anvil/Assets/_scripts/Utility/ObjectPooler.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour {
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    #region Singleton
+
+    public static ObjectPooler Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach(Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            // loops placing all objects in the pool and setting them to inactive
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    // SpawnFromPool //
+    // gets an object from the pool and dequeues it for activating and moving to position needed, then returns it to the
+    // object pool for future use if needed
+    public GameObject SpawnFromPool(string _tag, Vector3 _position, Quaternion _rot)
+    {
+        
+        if (!poolDictionary.ContainsKey(_tag))
+        {
+            Debug.Log("there isnt anything in the dictionary with tag: " + _tag);
+            return null;
+        }
+
+        GameObject objectToSpawn  = poolDictionary[_tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = _position;
+        objectToSpawn.transform.rotation = _rot;
+
+        poolDictionary[_tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+}
